ac03afe20c4bb4c3b1d67bd9bccd9a004a99aed7
Murali-Learner <muralikrishna2354@gmail.com>
2022-04-27 20:38:14 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/censor_controller/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.censor_controller
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/controller.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'package:separated_column/separated_column.dart';
+
+class Controller extends StatefulWidget {
+  const Controller({Key? key}) : super(key: key);
+
+  @override
+  _ControllerState createState() => _ControllerState();
+}
+
+class _ControllerState extends State<Controller> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.all(50),
+        color: Colors.black,
+        child: SeparatedColumn(
+          separatorBuilder: (BuildContext context, int index) => SizedBox(
+            height: 20,
+          ),
+          children: [
+            StreamBuilder(
+              stream: accelerometerEvents.cast(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                return Container(
+                    child: Center(
+                  child: Text(
+                    snapshot.hasData ? snapshot.data.toString() : "",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ));
+              },
+            ),
+            StreamBuilder(
+              stream: magnetometerEvents.cast(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                return Container(
+                    child: Center(
+                  child: Text(
+                    snapshot.hasData ? snapshot.data.toString() : "",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ));
+              },
+            ),
+            StreamBuilder(
+              stream: userAccelerometerEvents.cast(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                return Container(
+                    child: Center(
+                  child: Text(
+                    snapshot.hasData ? snapshot.data.toString() : "",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ));
+              },
+            ),
+            StreamBuilder(
+              stream: gyroscopeEvents.cast(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                return Container(
+                    child: Center(
+                  child: Text(
+                    snapshot.hasData ? snapshot.data.toString() : "",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
